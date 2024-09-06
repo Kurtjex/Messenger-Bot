bb766ab3fa08ef716bbdd843c8c899a078ca56cd
Ziggy-Github <162323407+Kurtjex@users.noreply.github.com>
2024-09-06 22:31:07 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -17,10 +17,10 @@ module.exports.run = async function({
 }) {
   const input = args.join(' ');
   if (!input) {
-    api.sendMessage(`Please provide a question firt!`, event.threadID, event.messageID);
+    api.sendMessage(`Please provide a question.`, event.threadID, event.messageID);
     return;
   }
-  api.sendMessage(`🖕 "${input}"🖕`, event.threadID, event.messageID);
+  api.sendMessage(`𝘼𝙣𝙨𝙬𝙚𝙧𝙞𝙣𝙜..."${input}"`, event.threadID, event.messageID);
   try {
     const {
       data
